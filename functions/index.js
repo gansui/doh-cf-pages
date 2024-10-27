bdb809a9b34bcc3bc7f293dc954d506a6296268c
gansui <138343088+gansui@users.noreply.github.com>
2024-10-27 22:43:00 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,31 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query/dns-query'
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+const doh = 'https://security.cloudflare-dns.com/dns-query'
+const dohjson = 'https://security.cloudflare-dns.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
-export const onRequestGet = async ({request}) => {
-	 const { method, headers, url } = request 
-	 const searchParams = new URL(url).searchParams
-	 if (searchParams.has('dns')) {
-	 return await fetch(doh + '?dns=' + searchParams.get('dns'), {
-            method: 'GET',
-            headers: {
-                'Accept': contype,
-            }
-        });
-	 } else if (method== 'GET' && headers.get('Accept')==jstontype) {
-        const search = new URL(url).search
-         return await fetch(dohjson + search, {
-            method: 'GET',
-            headers: {
-                'Accept': jstontype,
-            }
-        });
-    } else {
-        return new Response("", {status: 404})
-    }
-
-}
-
-export const onRequestPost = async ({ request }) => {
-	const { headers } = request 
-  if (headers.get('content-type')==contype) {
-        return fetch(doh, {
-            method: 'POST',
-            headers: {
-                'Accept': contype,
-                'Content-Type': contype,
-            },
+export default {
+	async fetch(request, env, ctx) {
+        const { headers } = request 
+ 		 if (headers.get('content-type')==contype) {
+        	return fetch(doh, {
+            	method: 'POST',
+            	headers: {
+                	'Accept': contype,
+                	'Content-Type': contype,
+            	},
             body: request.body,
         });
-		 } else {
-        return new Response("", {status: 404})
-    }
-}
+        } else {
+        	return new Response("", {status: 404})
+    	}
+	}
+  };
