d48b0820e9bf6d4d2c7f05e2f99ff5df7d1cb881
gansui <138343088+gansui@users.noreply.github.com>
2024-10-28 18:39:16 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-const doh = 'https://dns10.quad9.net/dns-query'
-const dohjson = 'https://dns10.quad9.net/dns-query'
+const doh = 'https://security.cloudflare-dns.com/dns-query'
+const dohjson = 'https://security.cloudflare-dns.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
-export default {
-	async fetch(request, env, ctx) {
-        const { headers } = request.headers
- 		if (headers.get('content-type')===contype) {
-        	const response = await fetch(doh, {
-            		method: 'POST',
-            		headers: headers,
-            		body: request.body,
-        	});
-		console.log(response);
-		return response;
-        } else {
-        	return new Response("", {status: 404})
-    	}
-	}
-  };
-  
-  
+export const onRequestGet = async ({request}) => {
+	 const { method, headers, url } = request 
+	 const searchParams = new URL(url).searchParams
+	 if (searchParams.has('dns')) {
+	 return await fetch(doh + '?dns=' + searchParams.get('dns'), {
+            method: 'GET',
+            headers: {
+                'Accept': contype,
+            }
+        });
+	 } else if (method== 'GET' && headers.get('Accept')==jstontype) {
+        const search = new URL(url).search
+         return await fetch(dohjson + search, {
+            method: 'GET',
+            headers: {
+                'Accept': jstontype,
+            }
+        });
+    } else {
+        return new Response("", {status: 404})
+    }
+
+}
+
+export const onRequestPost = async ({ request }) => {
+	const { headers } = request 
+  if (headers.get('content-type')==contype) {
+        return fetch(doh, {
+            method: 'POST',
+            headers: {
+                'Accept': contype,
+                'Content-Type': contype,
+            },
+            body: request.body,
+        });
+		 } else {
+        return new Response("", {status: 404})
+    }
+}
